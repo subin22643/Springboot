77653aca717e8561631cee798737721c67a40627
subin22643 <bin577733@naver.com>
2025-03-13 09:54:22 +0900

    init

---
+++ src/main/java/com/example/spharos6/Spharos6Application.java
@@ -0,0 +1,13 @@
+package com.example.spharos6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spharos6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spharos6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spharos6/Spharos6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spharos6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spharos6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
